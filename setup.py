a6c2bf6f948a5acc67d966d1adcb70debf0c4580
Aaron Hampton <aaron.hampt@gmail.com>
2015-09-24 01:33:53 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='fbacalculator',
+    packages=['fbacalculator'],
+    version='0.0.1',
+    description="Calcuate Amazon FBA fees",
+    author="Aaron Hampton",
+    author_email="aaron.hampt@gmail.com",
+    url="https://github.com/hamptus/fbacalculator",
+)
