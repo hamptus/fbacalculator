f43b4a51065c3a6a8a6ca1397ba48e587709468e
Aaron Hampton <aaron@spideroak-inc.com>
2016-09-30 22:02:44 -0400

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='fbacalculator',
     packages=['fbacalculator'],
-    version='0.0.2',
+    version='0.1.0',
     description="Calcuate Amazon FBA fees",
     author="Aaron Hampton",
     author_email="aaron.hampt@gmail.com",
